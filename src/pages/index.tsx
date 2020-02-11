b53482986969c5feefb1cfcb0109cec3bd19a0df
Mock <535718358@qq.com>
2020-02-11 18:12:02 +0800

    preare set eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Button } from 'antd';
 import styles from './index.less';
 
-export default function() {
+// function Test() {
+//   useEffect(() => {
+//     console.log(1);
+//   });
+
+//   return <div />;
+// }
+
+export default function () {
+  // const app = '1';
   return (
     <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>
-          To get started, edit <code>src/pages/index.js</code> and save to reload.
-        </li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">Getting Started</a>
-        </li>
-      </ul>
+      <Button>hello next Antd</Button>
     </div>
   );
 }
