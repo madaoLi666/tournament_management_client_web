9004b16ed57ac84bb8a942c2a5ab1224ba96b0de
Mock <535718358@qq.com>
2020-02-11 11:44:09 +0800

    删除原有

--- src/services/unit.ts
+++
@@ -1,9 +0,0 @@
-import axiosInstance from '@/utils/request.ts';
-
-export async function modifyUnitData(data: object): Promise<any> {
-    return axiosInstance.post('/basicunitdata/',data);
-}
-
-export async function modifyMainPart(data: object): Promise<any> {
-  return axiosInstance.post('/uploadbusinesslicense/', data);
-}
