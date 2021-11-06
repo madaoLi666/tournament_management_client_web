9a4bf10dffab2d3928d76e4fd322684483959ed3
jcedo <350477631@qq.com>
2021-11-07 00:17:17 +0800

    feat(amateurlevel): remove the second info in info page of amateurlevel

--- src/pages/AmateurLevel/index.tsx
+++ src/pages/AmateurLevel/index.tsx
@@ -7,21 +7,6 @@ import AmateurlevelSearch from '@/pages/AmateurLevel/AmateurLevelSearch';
 import { router } from 'umi';
 import styles from './index.less';
 
-const data_0913 = [
-  {
-    title: '培训班交通指引',
-    id: 0,
-  },
-  {
-    title: '粤轮协【2020】11号 关于举办2020年广东省滑板等级教练员培训班的通知',
-    id: 1,
-  },
-  {
-    title: '附件1：滑板教练员技术等级申请表',
-    id: 2,
-  },
-];
-
 const data_1008 = [
   {
     title: '粤轮协【2020】16号 关于举办《广东省轮滑技术水平等级测试（20201024期）》的通知',
@@ -44,7 +29,6 @@ const data_1008 = [
 function AmateurLevelIndex(props: any) {
   const { downloadFileList, dispatch } = props;
 
-  const [files_0913, setFiles_0913] = useState([]);
   const [files_1008, setFiles_1008] = useState([]);
 
   useEffect(() => {
@@ -56,7 +40,6 @@ function AmateurLevelIndex(props: any) {
 
   useEffect(() => {
     if (downloadFileList) {
-      setFiles_0913(downloadFileList.filter((m: any) => m.id >= 67 && m.id <= 69).map((v: any) => v.file));
       setFiles_1008(downloadFileList.filter((m: any) => m.id >= 70 && m.id <= 73).map((v: any) => v.file));
     }
   }, [downloadFileList])
@@ -85,24 +68,6 @@ function AmateurLevelIndex(props: any) {
           <List.Item>
             <Typography.Text strong>
               <a href={files_1008[item.id]}>[点击下载]</a>
-            </Typography.Text>{' '}
-            {item.title}
-          </List.Item>
-        )}
-      />
-      <br />
-      <List
-        header={
-          <div style={{ textAlign: 'center' }}>
-            <strong>关于举办 2020 年广东省滑板初级教练员培训班的通知</strong>
-          </div>
-        }
-        bordered
-        dataSource={data_0913}
-        renderItem={item => (
-          <List.Item>
-            <Typography.Text strong>
-              <a href={files_0913[item.id]}>[点击下载]</a>
             </Typography.Text>{' '}
             {item.title}
           </List.Item>
